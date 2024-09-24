98bf64e866f6bf8d3fb67f7c19f0e52073f0fd7e
Hermann Weinberger <miol@protonmail.com>
2024-09-24 13:36:20 +0200

    finish exercise_1_1

---
+++ exercise_1_1/script.js
@@ -0,0 +1,20 @@
+
+
+const sportsCar = {
+    modelId: 7,
+    brand: "SpyMotors",
+    model: "Spectre",
+    colors: ["Silver", "Black", "White"],
+
+};
+
+console.log(sportsCar);
+
+sportsCar.modelId = 8;
+
+console.log(sportsCar);
+
+sportsCar.colors.push("Midnight Blue");
+
+console.log(sportsCar);
+
